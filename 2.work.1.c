7cf06e78e4cbdde04b9c4acb05d99421ef7dc1b4
huangluhan <18836599920@163.com>
2024-12-19 14:18:18 +0800

    Homework4

--- 2.work.1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    long x = 1;
-    // 将字符串常量的地址赋值给x
-    ((char **)&x)[0] = "C!YYDS!";
-    printf("%s", (char *)x);
-
-    return 0;
-}
--- module.c
+++
@@ -1,9 +0,0 @@
-int global_variable = 10;
-
-int function1() {
-    return 20;
-}
-
-int function2() {
-    return 30;
-}
